d39daf10d9a4b459c7db408a18e7e2eeeff1bd8e
uiv21850 <uiv21850@vitesco.com>
2026-02-09 14:16:11 +0200

    test

--- test_tiny.c
+++ test_tiny.c
@@ -1,1 +1,48 @@
+#include <stdio.h>
+
 int test() { int x; x = 5; return x; }
+
+unsigned test2() { unsigned x; x = 5; return x; }
+
+unsigned long test3() { unsigned long x; x = 5; return x; }
+
+unsigned long long test4() { unsigned long long x; x = 5; return x; }
+
+float test5() { float x; x = 5.0; return x; }
+
+double test6() { double x; x = 5.0; return x; }
+
+float test7() { double x; x = 5; return x; }
+
+double test8() { float x; x = 5; return x; }
+
+int* ptr;
+
+struct Point { int x; int y; };
+
+struct Point p;
+
+struct Point* pptr;
+
+struct Point test9() { struct Point p; p.x = 1; p.y = 2; return p; }
+
+typedef struct empty{} emp;
+
+emp e1;
+
+int main() {
+
+    void* vp;
+    
+
+    if (pptr == NULL);
+
+
+    return 0;
+}
+
+
+
+
+
+
